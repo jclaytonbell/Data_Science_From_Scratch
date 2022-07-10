712273688dee985f600841daa3e43bfe692b0a06
J. Clayton Bell <75789437+jclaytonbell@users.noreply.github.com>
2022-07-10 15:30:43 -0500

    vector methods

--- linear_algebra/linear_algebra.py
+++ linear_algebra/linear_algebra.py
@@ -1,4 +1,5 @@
 # CHAPTER 4: LINEAR ALGEBRA
+import math
 
 class LinearAlgebra():
     """
@@ -7,12 +8,94 @@ class LinearAlgebra():
 
     def __init__(self):
         """
-        Initialize class
+        Initialize class.
         """
+
+    def distance(self, v, w):
+        """
+        Returns the distance between two vectors of the same dimensions, i.e., the magnitude of the vector created by
+        subtracting the two input vectors.
+
+        distance = sqrt( (v_1 - w_1) ^ 2 + (v_2 - w_2) ^ 2 + ..... + (v_n - w_n) ^ 2 )
+
+        """
+        return self.vector_magnitude(v=self.vector_subtract(v, w))
+
+    def squared_distance(self, v, w):
+        """
+        Returns the sum of the squared differences between two vectors of the same dimensions.
+
+        squared_distance = (v_1 - w_1) ^ 2 + (v_2 - w_2) ^ 2 + ..... + (v_n - w_n) ^ 2
+
+        """
+        return self.sum_of_squares(v=self.vector_subtract(v, w))
+
+    def vector_magnitude(self, v):
+        """
+        Returns the magnitude of a vector, i.e., the square root of the dot product of a vector and itself.
+
+        magnitude = sqrt(v_1 ^ 2 + v_2 ^ 2 + ..... + v_n ^ 2)
+
+        """
+        self._validate_vector_(vector=v)
+        return math.sqrt(self.sum_of_squares(v=v))
+
+    def sum_of_squares(self, v):
+        """
+        Returns the sum of squares of a vector, i.e., the dot product of a vector and itself.
+
+        sum_of_squares = v_1 ^ 2 + v_2 ^ 2 + ..... + v_n ^ 2
+
+        """
+        self._validate_vector_(vector=v)
+        return self.dot_product(v=v, w=v)
+
+    def dot_product(self, v, w):
+        """
+        Returns the dot product of two vectors, i.e., the sum of the products of the vector elements:
+
+        dot_product = v_1 * w_1 + v_2 * w_2 + ..... + v_n * w_n
+
+        """
+        self._validate_vectors_same_length_(vector_list=[v, w])
+        return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+    def vector_mean(self, vector_list):
+        """
+        Return a new vector created by summing a list of vectors of the same dimensions, and dividing each element by
+        the number of the input vectors (i.e., returns the average of each vector element)
+
+        n = len(vector_list)
+        x = [(v[0] + w[0]) / n, (v[1] + w[1]) / n,....(v[n] + w[n]) / n]
+
+        """
+        self._validate_vectors_same_length_(vector_list=vector_list)
+        n = len(vector_list)
+        return self.scalar_multiply(1 / n, self.vector_sum(vector_list=vector_list))
+
+    def scalar_multiply(self, c, v):
+        """
+        Returns the a new vector created by multiplying the input vector by a scalar.
+
+        x = [c * v[0], c * v[1],....c * v[n]]
+
+        """
+        self._validate_scalar_(scalar=c)
+        self._validate_vector_(vector=v)
+        return [c * v_i for v_i in v]
+
+    def vector_sum(self, vector_list):
+        """
+        Return a new vector created by summing a list of vectors of the same dimensions.
+        """
+        result = vector_list[0]
+        for v in vector_list[1:]:
+            result = self.vector_add(v=result, w=v)
+        return result
 
     def vector_add(self, v, w):
         """
-        Return a new vector created by the sum of two input vectors (v and w) of the same dimensions.
+        Return a new vector created by adding two input vectors (v and w) of the same dimensions.
 
         x = [v[0] + w[0], v[1] + w[1],....v[n] + w[n]]
 
@@ -20,6 +103,17 @@ class LinearAlgebra():
         vector_list = [v, w]
         self._validate_vectors_same_length_(vector_list=vector_list)
         return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+    def vector_subtract(self, v, w):
+        """
+        Return a new vector created by the difference of two input vectors (v and w) of the same dimensions.
+
+        x = [v[0] - w[0], v[1] - w[1],....v[n] - w[n]]
+
+        """
+        vector_list = [v, w]
+        self._validate_vectors_same_length_(vector_list=vector_list)
+        return [v_i - w_i for v_i, w_i in zip(v, w)]
 
     def _validate_vectors_same_length_(self, vector_list):
         """
@@ -30,14 +124,14 @@ class LinearAlgebra():
             if all(len(i) == a for i in vector_list):
                 return True
             else:
-                raise ValueError("Vectors are not the same length.")
+                raise AssertionError("Vectors are not the same length.")
 
     def _validate_vectors_(self, vector_list):
         """
         Return True if input is a list of valid vectors.
         """
         if isinstance(vector_list, list):
-            if all([self._validate_vector_(i) for i in vector_list]):
+            if all([self._validate_vector_(vector=i) for i in vector_list]):
                 return True
         else:
             raise ValueError("Vectors must be represented by a list of numeric values.")
@@ -52,4 +146,14 @@ class LinearAlgebra():
                 return True
         else:
             raise ValueError("Vectors must be represented by a list of numeric values.")
+
+    @staticmethod
+    def _validate_scalar_(scalar):
+        """
+        Return True if input is a number.
+        """
+        if isinstance(scalar, (int, float)):
+            return True
+        else:
+            raise ValueError("Scalars must be numeric values.")
 
--- test/test_vector.py
+++ test/test_vector.py
@@ -4,42 +4,102 @@ from linear_algebra.linear_algebra import LinearAlgebra
 V = [1.0, 2.5, 5, 7.5]
 W = [1, 3, 5, 7]
 X = [0, 1.2, 3.4, 5.6, 7.8]
+C = 2
 
 class Test_Polygon(unittest.TestCase):
 
     def setUp(self):
-        self.vector = LinearAlgebra()
+        self.linalg = LinearAlgebra()
 
+    def test_distance(self):
+        """Test distance method."""
+        self.assertTrue(self.linalg.distance(v=V, w=W), 0)
+        with self.assertRaises(AssertionError):
+            self.linalg.distance(v=V, w=X)
+
+    def test_squared_distance(self):
+        """Test squared_distance method."""
+        self.assertTrue(self.linalg.squared_distance(v=V, w=W), 0)
+        with self.assertRaises(AssertionError):
+            self.linalg.squared_distance(v=V, w=X)
+
+    def test_vector_magnitude(self):
+        """Test vector_magnitude method."""
+        self.assertTrue(round(self.linalg.vector_magnitude(v=V), 6), 9.407444)
+        with self.assertRaises(ValueError):
+            self.linalg.vector_magnitude(v=C)
+
+    def test_sum_of_squares(self):
+        """Test sum_of_squares method."""
+        self.assertTrue(self.linalg.sum_of_squares(v=V), 88.5)
+        with self.assertRaises(ValueError):
+            self.linalg.sum_of_squares(v=C)
+
+    def test_dot_product(self):
+        """Test dot_product method."""
+        self.assertTrue(self.linalg.dot_product(v=V, w=W), 86)
+        with self.assertRaises(AssertionError):
+            self.linalg.dot_product(v=V, w=X)
+
+    def test_vector_mean(self):
+        """Test vector_mean method."""
+        self.assertListEqual(self.linalg.vector_mean(vector_list=[V, W]), [1.0, 2.75, 5, 7.25])
+        with self.assertRaises(AssertionError):
+            self.linalg.vector_sum(vector_list=[V, X])
+
+    def test_scalar_multiply(self):
+        """Test scalar_multiply method."""
+        self.assertListEqual(self.linalg.scalar_multiply(c=2, v=V), [2.0, 5.0, 10, 15.])
+        with self.assertRaises(ValueError):
+            self.linalg.scalar_multiply(c='2', v=V)
+
+    def test_vector_sum(self):
+        """Test vector_sum method."""
+        self.assertListEqual(self.linalg.vector_sum(vector_list=[V, W]), [2.0, 5.5, 10, 14.5])
+        with self.assertRaises(AssertionError):
+            self.linalg.vector_sum(vector_list=[V, X])
+
+    def test_vector_add(self):
+        """Test vector_add method."""
+        self.assertListEqual(self.linalg.vector_add(v=V, w=W), [2.0, 5.5, 10, 14.5])
+        with self.assertRaises(AssertionError):
+            self.linalg.vector_add(v=V, w=X)
+
+    def test_vector_subtract(self):
+        """Test vector_subtract method."""
+        self.assertListEqual(self.linalg.vector_subtract(v=V, w=W), [0, -0.5, 0, 0.5])
+        with self.assertRaises(AssertionError):
+            self.linalg.vector_subtract(v=V, w=X)
+        
     def test_validate_vectors_same_length(self):
         """Test _validate_vectors_same_length_ method."""
-        self.assertTrue(self.vector._validate_vectors_same_length_([V, W]))
-        self.assertTrue(self.vector._validate_vectors_same_length_([V]))
-        with self.assertRaises(ValueError):
-            self.vector._validate_vectors_same_length_([V, X])
-            # self.vector._validate_vectors_(['2'])
-            # self.vector._validate_vectors_(1.234)
-            # self.vector._validate_vectors_(1)
-            # self.vector._validate_vectors_([])
-            # self.vector._validate_vectors_(None)
+        self.assertTrue(self.linalg._validate_vectors_same_length_([V, W]))
+        self.assertTrue(self.linalg._validate_vectors_same_length_([V]))
+        with self.assertRaises(AssertionError):
+            self.linalg._validate_vectors_same_length_([V, X])
 
     def test_validate_vectors_(self):
         """Test _validate_vectors_ method."""
-        self.assertTrue(self.vector._validate_vectors_([V, X]))
+        self.assertTrue(self.linalg._validate_vectors_([V, X]))
         with self.assertRaises(ValueError):
-            self.vector._validate_vectors_([V, 'a'])
-            self.vector._validate_vectors_(['2'])
-            self.vector._validate_vectors_(1.234)
-            self.vector._validate_vectors_(1)
-            self.vector._validate_vectors_([])
-            self.vector._validate_vectors_(None)
+            self.linalg._validate_vectors_([V, 'a'])
 
     def test_validate_vector_(self):
         """Test _validate_vector_ method."""
-        self.assertTrue(self.vector._validate_vector_(V))
+        self.assertTrue(self.linalg._validate_vector_(V))
         with self.assertRaises(ValueError):
-            self.vector._validate_vector_('a')
-            self.vector._validate_vector_(['2'])
-            self.vector._validate_vector_(1.234)
-            self.vector._validate_vector_(1)
-            self.vector._validate_vector_([])
-            self.vector._validate_vector_(None)
+            self.linalg._validate_vector_('a')
+            self.linalg._validate_vector_(['2'])
+            self.linalg._validate_vector_(1.234)
+            self.linalg._validate_vector_(1)
+            self.linalg._validate_vector_([])
+            self.linalg._validate_vector_(None)
+
+    def test_validate_scalar_(self):
+        """Test _validate_scalar_ method."""
+        self.assertTrue(self.linalg._validate_scalar_(C))
+        with self.assertRaises(ValueError):
+            self.linalg._validate_vector_('a')
+            self.linalg._validate_vector_(['2'])
+            self.linalg._validate_vector_([])
+            self.linalg._validate_vector_(None)
